a9047df26d2d43155e9ffe2bb8d1e5bafcc4dee9
Sholto Maud <sholto.maud@gmail.com>
2016-06-18 14:07:10 +1000

    landed colors from config

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ kgo('required', function (done) {
     done(msg)
   }
   let config = require(path.join(process.cwd(), '/', argv.c))
-
   config.output = (argv.o) ? path.join(process.cwd(), '/', argv.o) : ((config.dest) ? path.join(process.cwd(), '/', config.dest) : path.join(process.cwd(), '/assets'))
 
   config.tmp = (config.temp) ? path.join(process.cwd(), '/', config.temp) : path.join(process.cwd(), '/temp')
@@ -49,21 +48,36 @@ kgo('required', function (done) {
   })
 })('copy', ['required', 'search'], function (config, files, done) {
   // files.forEach(function (source, index, array) {
+  const length = Object.keys(files).length
+  const tempFiles = {}
+  let i = 1
+
   for (var source in files) {
-    if (p.hasOwnProperty(source)) {
+    let last = i === length // true if last, false if not last
+    // debug('last', last, 'i', i, 'length', length, 'source', source, 'file ', files[source])
+
+    if (!files.hasOwnProperty(source)) {
+      debug('hasOwnProperty')
       continue
     }
-    var target = path.join(config.tmp, '/', path.basename(source))
+
+    let target = path.join(config.tmp, '/', path.basename(source))
+    tempFiles[target] = files[source]
+
     debug('kgo:copy clobbering target file', target)
     fs.copySync(source, target, {clobber: true}, function (err) {
       if (err) done(err)
     })
-    if (index === array.length - 1) {
+    // if (index === array.length - 1) {
+
+    if (last) {
       debug(' - done')
-      done(null)
+      done(null, tempFiles)
     }
-  })
-})('color', ['required', 'search', 'copy'], function (config, files, copy, done) {
+    i++
+  // })
+  }
+})('color', ['required', 'copy'], function (config, tempFiles, done) {
   debug('kgo:color')
 
   fs.walk(config.tmp)
@@ -72,19 +86,46 @@ kgo('required', function (done) {
     while ((item = this.read())) {
       if (item.stats.isFile()) {
         let file = item.path
+  // const length = Object.keys(tempFiles).length
+  // let i = 1
+  // for (var file in tempFiles) {
+    // let last = i === length // true if last, false if not last
+        debug('file: ', file)
+
+    // if (!tempFiles.hasOwnProperty(file)) {
+    //   debug('hasOwnProperty')
+    //   continue
+    // }
+
         fs.readFile(file, 'utf8', function (err, data) {
           if (err) done(err)
+
+          debug('readFile: ', file)
+
           parser.parseString(data, function (err, result) {
             if (err) debug(err)
             let svgPath = result.svg.path
             svgPath.forEach(function (svg, index, array) {
               // if (!svg.$.fill) result.svg.path[index].$.fill = config.fill
-              if (!svg.$.fill) result.svg.path[index].$.fill = files[file].fill
+
+              debug('file: ', file)
+              debug('fill: ', tempFiles[file].fill)
+
+              if (!svg.$.fill) result.svg.path[index].$.fill = tempFiles[file].fill
             })
             let xml = builder.buildObject(result)
             fs.writeFileSync(file, xml, 'utf8')
+
+        // if (last) {
+        //   debug(' - done')
+        //   done(null, tempFiles)
+        // }
+        // i++
           })
         })
+  // }
+
+
       }
     }
   })
@@ -144,13 +185,17 @@ kgo('required', function (done) {
 function processIcons (icons, callback) {
   // let files = []
   let files = {}
+  let fileCheck = {}
+
   debug('processIcons')
   finder.on('file', function (file, stat) {
     icons.forEach(function (mdi, index, array) {
       let fileName = 'ic_' + mdi.icon + '_' + (mdi.size || '24px') + '.svg'
-      if (fileName === path.basename(file)) {
+      if (fileName === path.basename(file) && !fileCheck[fileName]) {
         // files.push(file)
-        files[fileName] = mdi.icon
+        files[file] = mdi
+      } else {
+        fileCheck[fileName]
       }
     })
   })
